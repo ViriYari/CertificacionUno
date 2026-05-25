8c2eab70fd612b8eea47264c8d9ed32d14b17410
ViriYari <57279430+ViriYari@users.noreply.github.com>
2026-05-24 22:48:58 -0600

    fix: Funcionamiento headless

--- demo/src/test/java/base/BaseTest.java
+++ demo/src/test/java/base/BaseTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
 
@@ -12,9 +13,25 @@ public class BaseTest {
     protected WebDriver driver;
 
     @BeforeEach
-    void setUp() {
+    //Funcione sin abrir la ventana
+    /*void setUp() {
 
         driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        driver.manage().timeouts()
+                .implicitlyWait(Duration.ofSeconds(5));
+    }*/
+  
+    void setUp() {
+        // Configuraciones para que Chrome funcione en la nube (GitHub Actions)
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new"); // Ejecuta sin abrir ventana
+        options.addArguments("--no-sandbox");   // Evita problemas de seguridad en Linux
+        options.addArguments("--disable-dev-shm-usage"); // Optimiza el uso de memoria
+
+        driver = new ChromeDriver(options); // Pasamos las opciones aquí
 
         driver.manage().window().maximize();
 
